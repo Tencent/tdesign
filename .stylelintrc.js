9ead44c03754d44fbda2b0696c07c681de12c469
fennghuang <89014758+fennghuang@users.noreply.github.com>
2023-09-14 08:29:08 +0000

    chore: add eslint prettier stylelint rule

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-standard-less'],
+  rules: {
+    'number-leading-zero': 'never',
+    'color-function-notation': 'legacy',
+    'alpha-value-notation': 'number',
+  },
+};
