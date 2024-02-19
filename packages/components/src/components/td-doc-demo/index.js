a941358bde97d5b9100ed5597367e09555db75aa
wū yāng <uyarnchen@gmail.com>
2024-02-19 18:22:50 +0800

    feat: support various demos (#447)
    * feat: support ts demo
    
    * feat: support various demo display
    
    * feat: support various demo display
    
    * feat: demo tabs
    
    * feat: demo tabs
    
    * feat: export currentRenderCode

--- packages/components/src/components/td-doc-demo/index.js
+++ packages/components/src/components/td-doc-demo/index.js
@@ -1,4 +1,4 @@
-import { html, define } from 'hybrids';
+import { html, define, dispatch } from 'hybrids';
 import Prism from 'prismjs';
 import style from './style.less';
 import codeIcon from '@images/code.svg?raw';
@@ -14,6 +14,9 @@ export default define({
   language: 'jsx',
   showCode: false,
   mode: 'auto', // auto open
+  currentLangIndex: 0,
+  languages: undefined, // multiple languages display
+  currentRenderCode: '',
   theme: {
     get: (host, lastValue) => lastValue || sessionStorage.getItem('--tdesign-theme') || 'light',
     set: (host, value) => value,
@@ -29,16 +32,65 @@ export default define({
       return () => window.removeEventListener('storageChange', themeChange);
     },
   },
-  render: (host) => {
-    const {
-      code, language, showCode, mode, theme,
-    } = host;
-    const highlightCode = Prism.highlight(code, Prism.languages[language], language);
+  activeStyleMap: {
+    get: (_host, lastValue) => lastValue || undefined,
+    set: (_host, value) => value,
+    connect: (host, key) => {
+      function handleResize() {
+        if (!host.shadowRoot) {
+          setTimeout(handleResize, 300);
+          return;
+        }
 
+        const items = host.shadowRoot.querySelectorAll('.TDesign-doc-demo-tabs__item');
+        let styleMap = {};
+        items.forEach((item) => {
+          if (!item.offsetWidth) {
+            styleMap = null;
+          } else {
+            const { tab } = item.dataset;
+            styleMap[tab] = {
+              width: `${item.offsetWidth}px`,
+              transform: `translate3d(${item.offsetLeft - 4}px, 0, 0)`,
+            };
+          }
+        });
+        Object.assign(host, { [key]: styleMap });
+      }
+
+      requestAnimationFrame(handleResize);
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    },
+  },
+  render: (host) => {
+    let { code, language, showCode, mode, theme, currentLangIndex, languages, activeStyleMap } = host;
+    const languageArr = typeof languages === 'string' ? languages.split(',') : [];
+
+    const tabsDisplay =
+      languageArr.length > 0 &&
+      languageArr.filter((lang) => host.dataset?.[lang] || host.dataset?.[lang.toLocaleLowerCase()]).length ==
+        languageArr.length;
+
+    const currentLang = languageArr[currentLangIndex] || '';
+    const currentCode = host.dataset?.[currentLang] || host.dataset?.[currentLang.toLocaleLowerCase()] || code;
+    const highlightCode = Prism.highlight(currentCode, Prism.languages[language], language);
+    host.currentRenderCode = currentCode;
+    const activeStyle = activeStyleMap && languageArr.length ? activeStyleMap[languageArr[currentLangIndex]] : {};
     const showCodeStyle = {
       transitionDuration: '.2s',
       maxHeight: showCode ? '560px' : 0,
       transitionTimingFunction: showCode ? 'cubic-bezier(.82, 0, 1, .9)' : 'ease',
+    };
+
+    const handleClick = (index) => {
+      host.currentLangIndex = index;
+
+      dispatch(host, 'click', { detail: { index, lang: languageArr[index] } });
     };
 
     return html`
@@ -48,10 +100,36 @@ export default define({
           <div class="TDesign-doc-demo__btns">
             <slot name="action"></slot>
             <td-doc-copy code=${code} theme=${mode === 'open' ? 'dark' : 'light'}></td-doc-copy>
-            ${mode === 'open' ? html`` : html`<span class="action code ${showCode ? 'active' : ''}" onclick=${html.set('showCode', !showCode)} innerHTML=${codeIcon}></span>`}
+            ${
+              mode === 'open'
+                ? html``
+                : html`<span
+                    class="action code ${showCode ? 'active' : ''}"
+                    onclick=${html.set('showCode', !showCode)}
+                    innerHTML=${codeIcon}
+                  ></span>`
+            }
           </div>
           <div class="TDesign-doc-demo__code ${theme}" style="${showCodeStyle}">
-            <pre class="language-${language}"><code class="language-${language}" innerHTML="${highlightCode}"></code></pre>
+          ${
+            tabsDisplay
+              ? html`<div class="TDesign-doc-demo-tabs">
+                  <span class="TDesign-doc-demo-tabs__active" style="${activeStyle}"></span>
+                  ${languageArr.map(
+                    (language, index) =>
+                      html`<div
+                        data-tab=${language}
+                        class="TDesign-doc-demo-tabs__item ${currentLangIndex === index ? 'active' : null}"
+                        onclick=${() => handleClick(index)}
+                      >
+                        ${language}
+                      </div>`,
+                  )}
+                </div>`
+              : ''
+          }
+              <pre class="language-${language}"><code class="language-${language}" innerHTML="${highlightCode}"></code></pre>
+            </div>
           </div>
         </div>
       </div>
