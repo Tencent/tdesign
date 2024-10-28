8ee4c674d01e52479a69aa315fce8c254f0b9202
阿Cai <jimmyrss1102@gmail.com>
2024-10-28 15:46:50 +0800

    chore: use `raw.githubusercontent`

--- scripts/auto-release.ts
+++ scripts/auto-release.ts
@@ -1,48 +1,39 @@
-import { execSync } from 'child_process';
-import fs from 'fs';
 import * as github from '@actions/github';
 
 const submodules = {
-  "tdesign-miniprogram": {
-    "title": "## Miniprogram for WeChat 发布",
-    "url": "https://github.com/Tencent/tdesign-miniprogram.git",
-    "changelog": "tdesign-miniprogram/CHANGELOG.md"
-  },
-  "tdesign-mobile-vue": {
-    "title": "## Vue3 for Mobile 发布",
-    "url": "https://github.com/Tencent/tdesign-mobile-vue.git",
-    "changelog": "tdesign-mobile-vue/CHANGELOG.md"
-  },
-  "tdesign-flutter": {
-    "title": "## Flutter for Mobile 发布",
-    "url": "https://github.com/Tencent/tdesign-flutter.git",
-    "changelog": "tdesign-flutter/tdesign-site/CHANGELOG.md"
-  },
-  "tdesign-mobile-react": {
-    "title": "## React for Mobile 发布",
-    "url": "https://github.com/Tencent/tdesign-mobile-react.git",
-    "changelog": "tdesign-mobile-react/CHANGELOG.md"
-  },
   "tdesign-vue": {
     "title": "## Vue2 for Web 发布",
-    "url": "https://github.com/Tencent/tdesign-vue.git",
-    "changelog": "tdesign-vue/CHANGELOG.md"
+    "changelogUrl": "https://raw.githubusercontent.com/Tencent/tdesign-vue/refs/heads/main/CHANGELOG.md"
   },
   "tdesign-vue-next": {
     "title": "## Vue3 for Web 发布",
-    "url": "https://github.com/Tencent/tdesign-vue-next.git",
-    "changelog": "tdesign-vue-next/CHANGELOG.md"
+    "changelogUrl": "https://raw.githubusercontent.com/Tencent/tdesign-vue-next/refs/heads/main/CHANGELOG.md"
   },
   "tdesign-react": {
     "title": "## React for Web 发布 ",
-    "url": "https://github.com/Tencent/tdesign-react.git",
-    "changelog": "tdesign-react/CHANGELOG.md"
-  }
-}
+    "changelogUrl": "https://raw.githubusercontent.com/Tencent/tdesign-react/refs/heads/main/CHANGELOG.md"
+  },
+  "tdesign-miniprogram": {
+    "title": "## Miniprogram for WeChat 发布",
+    "changelogUrl": "https://raw.githubusercontent.com/Tencent/tdesign-miniprogram/refs/heads/main/CHANGELOG.md"
+  },
+  "tdesign-mobile-vue": {
+    "title": "## Vue3 for Mobile 发布",
+    "changelogUrl": "https://raw.githubusercontent.com/Tencent/tdesign-mobile-vue/refs/heads/main/CHANGELOG.md"
+  },
+  "tdesign-flutter": {
+    "title": "## Flutter for Mobile 发布",
+    "changelogUrl": "https://raw.githubusercontent.com/Tencent/tdesign-flutter/refs/heads/main/tdesign-site/CHANGELOG.md"
+  },
+  "tdesign-mobile-react": {
+    "title": "## React for Mobile 发布",
+    "changelogUrl": "https://raw.githubusercontent.com/Tencent/tdesign-mobile-react/refs/heads/main/CHANGELOG.md"
+  },
+};
 
 type SubmoduleItem = keyof typeof submodules;
 
-const submodulesArr: SubmoduleItem[] = [
+const submodulesKeys: SubmoduleItem[] = [
   'tdesign-vue',
   'tdesign-vue-next',
   'tdesign-react',
@@ -51,14 +42,6 @@ const submodulesArr: SubmoduleItem[] = [
   'tdesign-mobile-react',
   'tdesign-flutter',
 ]
-
-
-/**
- * @description 检查文件是否存在
- * @param {string} filePath 
- * @returns {boolean}
- */
-const checkFileExist = (filePath: string) => fs.existsSync(filePath);
 
 /**
  * @description 获取内容
@@ -95,28 +78,6 @@ function getProjectDesc(project: SubmoduleItem, tag: string, prefixDesc: any, su
 
 // 开始执行
 
-execSync(`git submodule init`);
-
-submodulesArr.forEach(project => {
-  const url = submodules[project]['url'];
-
-  console.log(`\n`);
-  console.log(`将要访问的是${project}子模块，地址是${url}.`);
-  try {
-    if (checkFileExist(project)) {
-      console.log(`${project} 已经存在,将更新子模块.`);
-      execSync(`git submodule update --remote ${project}`);
-      return;
-    }
-
-    console.log(`${project} 不存在,将添加子模块,${url}.`);
-    execSync(`git submodule add ${url} ${project}`);
-  } catch (error) {
-    console.error(`Failed to add submodule ${project}:`, error);
-  }
-});
-
-
 /**
  * @description 检查日期是否在指定日期范围内
  *  在每个月的1号、8号、15号、22号生成
@@ -143,12 +104,18 @@ if (today.getDate() === 1) {
 }
 console.log(`今天是:${today},开始日期:${START_DATE},结束日期:${END_DATE}`);
 
-let output = '';
-submodulesArr.forEach(project => {
+const fetchPromises = submodulesKeys.map(async project => {
+  console.log(`开始处理${project}项目`);
+  const changelogUrl = submodules[project].changelogUrl;
+  console.log(changelogUrl);
   // 读取 CHANGELOG.md 文件内容
-  const changelogContent = fs.readFileSync(`./${submodules[project]['changelog']}`, 'utf8');
+  const response = await fetch(changelogUrl);
+  const changelogContent = await response.text();
+
   // 使用正则表达式提取版本标题行，确保以"##"开头并以日期结尾
   const versions = changelogContent.match(/^##.+?`\d{4}-\d{2}-\d{2}`\s*$/gm);
+
+  const projectOutput: string[] = [];
 
   versions?.forEach((versionLine) => {
     const dateMatch = versionLine.match(/\d{4}-\d{2}-\d{2}/);
@@ -162,64 +129,74 @@ submodulesArr.forEach(project => {
       const date = new Date(dateMatch[0]);
       // 检查日期是否在指定范围内,[2024-06-01, 2024-06-07] 闭区间
       if (date >= new Date(START_DATE) && date <= new Date(END_DATE)) {
-
         const desc = getProjectDesc(project, tag[0], submodules[project]['title'], '详情见：');
-        output += desc[0] + '\n' + versionContent + '\n' + desc[1] + '\n';
+        projectOutput.push(`${desc[0]}\n${versionContent}\n${desc[1]}\n`);
       }
     }
   });
+
+  return projectOutput.join('\n');
 });
 
-// 标题
-const monthShort = today.toLocaleString('en-US', { month: 'short' });
-const year = today.getFullYear();
-let times = "1st";
+Promise.all(fetchPromises).then(projectOutputs => {
+  const output = projectOutputs.join('\n');
 
-console.log('today', today.getDate());
-switch (today.getDate()) {
-  case 1:
-    times = "4th";
-    break;
-  case 8:
-    times = "2nd";
-    break;
-  case 15:
-    times = "3rd";
-    break;
-  case 22:
-    times = "4th";
-    break;
-  default:
-    times = "1st";
-    break;
-}
+  // 标题
+  const monthShort = today.toLocaleString('en-US', { month: 'short' });
+  const year = today.getFullYear();
+  let times = "1st";
 
-const tag = `v${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}`;
-const title = `TDesign Weekly Release (${monthShort} ${times} ${year})`;
+  console.log('today', today.getDate());
+  switch (today.getDate()) {
+    case 1:
+      times = "4th";
+      break;
+    case 8:
+      times = "1st";
+      break;
+    case 15:
+      times = "2nd";
+      break;
+    case 22:
+      times = "3rd";
+      break;
+    default:
+      times = "1st";
+      break;
+  }
 
-// 发布release
+  const tag = `v${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}`;
+  const title = `TDesign Weekly Release (${monthShort} ${times} ${year})`;
 
-const githubToken = process.env.GITHUB_TOKEN;
+  console.log('tag', tag);
+  console.log('title', title);
+  console.log('output', output);
 
-if (!githubToken) {
-  throw new Error('GITHUB_TOKEN is undefined');
-}
+  // 发布release
 
-console.log('tag', tag);
-console.log('title', title);
-console.log('output', output);
-console.log('GitHub Token:', githubToken);
-const octokit = github.getOctokit(githubToken);
-console.log('身份验证成功');
+  const githubToken = process.env.GITHUB_TOKEN;
 
-try {
-  octokit.rest.repos.createRelease({
-    owner: "Tencent",
-    repo: "tdesign",
-    tag_name: tag, // 添加缺少的 tag_name 参数
-    name: title, // 可选参数，使用 title 作为发布名称
-    body: output, // 可选参数，发布说明内容
-  })
-} catch (error) {
-  console.error("Error:", error);
-}  
+  if (!githubToken) {
+    throw new Error('GITHUB_TOKEN is undefined');
+  }
+
+  console.log('tag', tag);
+  console.log('title', title);
+  console.log('output', output);
+  console.log('GitHub Token:', githubToken);
+  const octokit = github.getOctokit(githubToken);
+  console.log('身份验证成功');
+
+  try {
+    octokit.rest.repos.createRelease({
+      owner: "Tencent",
+      repo: "tdesign",
+      tag_name: tag, // 添加缺少的 tag_name 参数
+      name: title, // 可选参数，使用 title 作为发布名称
+      body: output, // 可选参数，发布说明内容
+      draft: true, // 可选参数，是否为草稿
+    })
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
