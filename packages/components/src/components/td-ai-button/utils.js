af19eba54e4b255aaeff4b2309cf9cf6bb492735
Uyarn <uyarnchen@gmail.com>
2025-02-27 20:39:03 +0800

    chore: optimize

--- packages/components/src/components/td-ai-button/utils.js
+++ packages/components/src/components/td-ai-button/utils.js
@@ -9,7 +9,7 @@ let regExp = {
 
 let frameworkKeys = {
   react: 'tdesign-react',
-  vue: 'tdesign-vue-next',
+  vue: 'tdesign-vue-next,vue3',
   miniprogram: 'tdesign-miniprogram',
 };
 
@@ -18,7 +18,7 @@ const appendChildAsync = (parent, child) => {
     setTimeout(() => {
       parent.appendChild(child);
       resolve();
-    }, 200);
+    }, 100);
   });
 };
 
@@ -93,7 +93,7 @@ const createTooltips = (generateDemo, selectedText) => {
     unmountTooltips();
     let prompt = '';
     if (generateDemo) {
-      prompt = `请为我生成${component}的${selectedText}的代码示例`;
+      prompt = `请为我生成 ${component} 组件的 ${selectedText} 的 script setup 代码示例`;
     } else {
       prompt = component ? `请为我解释${component}的${selectedText}的定义` : `请为我解释${selectedText}的定义`;
     }
