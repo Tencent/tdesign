9c3b91d7a84222ea7987c1b6b8471979d1fdf49d
Rylan <rylanbot@qq.com>
2025-03-20 20:29:20 +0800

    chore(theme-generator): split shadow styles into files

--- packages/theme-generator/src/common/built-in/theme-map.js
+++ packages/theme-generator/src/common/built-in/theme-map.js
@@ -1,16 +1,18 @@
-import WebTDesignLight from '!raw-loader!../built-in/themes/web/TDesign/light.css';
-import WebTDesignDark from '!raw-loader!../built-in/themes/web/TDesign/dark.css';
-import WebTCloudLight from '!raw-loader!../built-in/themes/web/TCloud/light.css';
 import WebTCloudDark from '!raw-loader!../built-in/themes/web/TCloud/dark.css';
 import WebTCloudExtra from '!raw-loader!../built-in/themes/web/TCloud/extra.css';
+import WebTCloudLight from '!raw-loader!../built-in/themes/web/TCloud/light.css';
+import WebTDesignDark from '!raw-loader!../built-in/themes/web/TDesign/dark.css';
+import WebTDesignLight from '!raw-loader!../built-in/themes/web/TDesign/light.css';
 import WebFont from '!raw-loader!../built-in/themes/web/common/font.css';
 import WebRadius from '!raw-loader!../built-in/themes/web/common/radius.css';
+import WebShadow from '!raw-loader!../built-in/themes/web/common/shadow.css';
 import WebSize from '!raw-loader!../built-in/themes/web/common/size.css';
 
-import MobileTDesignLight from '!raw-loader!../built-in/themes/mobile/TDesign/light.css';
 import MobileTDesignDark from '!raw-loader!../built-in/themes/mobile/TDesign/dark.css';
+import MobileTDesignLight from '!raw-loader!../built-in/themes/mobile/TDesign/light.css';
 import MobileFont from '!raw-loader!../built-in/themes/mobile/common/font.css';
 import MobileRadius from '!raw-loader!../built-in/themes/mobile/common/radius.css';
+import MobileShadow from '!raw-loader!../built-in/themes/mobile/common/shadow.css';
 import MobileSpacer from '!raw-loader!../built-in/themes/mobile/common/spacer.css';
 
 /**
@@ -30,6 +32,7 @@ export const BUILT_IN_THEMES = {
     common: {
       font: WebFont,
       radius: WebRadius,
+      shadow: WebShadow,
       size: WebSize,
     },
   },
@@ -41,6 +44,7 @@ export const BUILT_IN_THEMES = {
     common: {
       font: MobileFont,
       radius: MobileRadius,
+      shadow: MobileShadow,
       spacer: MobileSpacer,
     },
   },
