7304c18a84eb72e6fa902c26ade7e7235e42a784
Uyarn <uyarnchen@gmail.com>
2025-09-08 20:08:17 +0800

    chore: optimize

--- site/src/pages/icons/index.vue
+++ site/src/pages/icons/index.vue
@@ -1,22 +1,3 @@
 <template>
-  <div>
-    <td-icons-view />
-    <td-doc-footer :style="footerStyle" />
-  </div>
+  <td-icons-view />
 </template>
-<script>
-export default {
-  name: 'icons',
-  computed: {
-    footerStyle() {
-      return {
-        '--content-padding-right': '0',
-        '--content-max-width': '1440px',
-        '--content-padding-left-right': '48px',
-        '--footer-inner-position': 'relative',
-        '--footer-logo-position': 'unset',
-      };
-    },
-  },
-};
-</script>
