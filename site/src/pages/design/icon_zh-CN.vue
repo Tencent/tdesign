c070940f7884d22bbb0ba77168a50bebcd07352f
Uyarn <uyarnchen@gmail.com>
2025-11-26 02:29:26 +0800

    feat: update icon design page and add tds link

--- site/src/pages/design/icon_zh-CN.vue
+++ site/src/pages/design/icon_zh-CN.vue
@@ -15,8 +15,8 @@
 
     <h2>概述</h2>
     <p>
-      Icon 作为 UI 构成中重要的元素，它一定程度上影响整体 UI 界面呈现出的风格，TDesign
-      初期提供一套适用于中后台场景的线性 Icon，以普适、通用的标准进行设计，风格契合默认 TDesign 的风格，线性、圆角。
+      Icon 作为 UI 构成中重要的元素，它一定程度上影响整体 UI 界面呈现出的风格，TDesign 提供一套适用于中后台场景的线性
+      Icon，以普适通用的标准进行设计，契合 TDesign 设计系统的整体风格。
     </p>
 
     <h2>原则</h2>
@@ -42,13 +42,14 @@
     <h3>栅格规范</h3>
     <p>
       栅格作为图表绘制的底层结构，是一切属性设计的基础。线条的长短粗细、图标的大小比例等关键因素均在其基础上制定。图标常见尺寸为16*16；20*20；24*24；32*32这四种输出尺寸。这些尺寸均可以清晰的显示在常规的显示器上。TDesign
-      最终选择以16*16px 的尺寸作为图标绘制的统一栅格尺寸。
+      最终选择以 24*24px 的尺寸作为图标绘制的统一栅格尺寸，经矢量转曲后等比缩放到 16*16px 交付。此方案旨在兼容 1.0
+      版本组件的默认调用尺寸，避免了额外调整图标大小的问题。
     </p>
     <img src="./assets/icon/grid-1.png" alt="" />
     <hr />
 
     <p>
-      由于屏幕像素的特性，非整数像素将会以灰度显示，那样将影响图标的清晰度以及整体的含义表达，所以在栅格内绘制时应尽量对齐像素点，最大程度上保证图标的清晰度。
+      受屏幕像素栅格化特性影响，非整数像素描边会因抗锯齿处理产生半透明灰度，导致图标边缘模糊、表意失真。因此，绘制时应严格对齐像素网格，以最大化保证图标的清晰度。
     </p>
     <img src="./assets/icon/grid-2.png" alt="" />
 
@@ -69,9 +70,8 @@
     <h3>画布与辅助线</h3>
     <p>
       画布作为图标设计的实际操作区域起到了控制画面、限制大小、调整间距等作用。在 TDesign
-      图标的设计上实际画布应控制在栅格中心14*14px的区域，周围留有1px的出血。在一些特殊情况下如 icon
-      过长或者有突出的边角等，允许内容适当延展，以确保图标视觉重量上的统一（ 超出部分建议为多边形的边角，并不宜超过
-      0.5px ）
+      图标的设计上实际画布应控制在栅格中心20*20px 的区域。在一些特殊情况下如 icon
+      过长或者有突出的边角等，允许内容适当延展，以确保图标视觉重量上的统一。
     </p>
     <img src="./assets/icon/canvas-1.png" alt="" />
     <hr />
@@ -82,7 +82,7 @@
     <img src="./assets/icon/canvas-2.png" alt="" />
     <hr />
 
-    <p>根据绘制对象的形象特征去选择辅助线，在必要时，可以将内容扩展到辅助线之外</p>
+    <p>根据绘制对象的形象特征去选择辅助线，在必要时，可以将内容扩展到辅助线之外，不应为了对齐辅助线而妥协图形设计。</p>
     <div class="legend">
       <div class="item">
         <img src="./assets/icon/canvas-3-left.png" />
@@ -96,49 +96,43 @@
     </div>
     <hr />
 
-    <p>在绘制一些具有指向性的图标时，建议在其指引的方向多预留 0.25px 的空间，以便平衡画面</p>
-    <div class="legend">
-      <div class="item">
-        <img src="./assets/icon/canvas-4-left.png" />
-        <img class="tag" src="https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/site/doc/good.png" />
-      </div>
-
-      <div class="item">
-        <img src="./assets/icon/canvas-4-right.png" />
-        <img class="tag" src="https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/site/doc/bad.png" />
-      </div>
-    </div>
-
-    <div class="legend">
-      <div class="item">
-        <img src="./assets/icon/canvas-4-left-bottom.png" />
-        <img class="tag" src="https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/site/doc/good.png" />
-      </div>
-
-      <div class="item">
-        <img src="./assets/icon/canvas-4-right-bottom.png" />
-        <img class="tag" src="https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/site/doc/bad.png" />
-      </div>
-    </div>
+    <p>在绘制一些具有指向性的图标时，建议在其指引的反方向延伸微量（ 0.25px 倍数）的视觉内容，以便平衡画面。</p>
+    <img src="./assets/icon/canvas-4.png" alt="" />
 
     <h3>线条</h3>
-    <p>绘制时建议使用1px宽度的笔画，这样以便在小尺寸的栅格内保证更多的样式可能性，在 16px 时为 1px， 24px 时等比加粗</p>
+    <p>
+      考虑通用普适性，本套系统中所有图标均设置为 2px 宽度的描边，并支持在<t-link
+        theme="primary"
+        href="/icons"
+        target="blank"
+        >🔗 TDesign 图标独立站点</t-link
+      >上进行全量化调节。
+    </p>
     <img src="./assets/icon/line-1.png" alt="" />
+    <img src="./assets/icon/line-2.png" alt="" style="margin-top: 16px" />
+
     <hr />
 
-    <p>在特殊情况下如复合图形，由于该类图标结构更为复杂，线条的粗细可以适当调整</p>
-    <img src="./assets/icon/line-2.png" alt="" />
-    <hr />
-
-    <p>在处理长线线条的长度时建议使用 2 的倍数，这样更易进行对称处理。而短线上建议使用 3 个像素</p>
+    <p>在处理长线线条的长度时建议使用 2 的倍数，这样更易进行对称处理。</p>
     <img src="./assets/icon/line-3.png" alt="" />
     <hr />
 
-    <p>线段末端直角处理，但在表现一些文字类图形或带有三维空间的透视图形时应与栅格相切</p>
-    <img src="./assets/icon/line-4.png" alt="" />
+    <p>线段末端直角处理，但在表现一些文字类图形或带有三维空间的透视图形时应与栅格相切。</p>
+    <div class="legend">
+      <div class="item">
+        <img src="./assets/icon/line-4-left.png" />
+        <img class="tag" src="https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/site/doc/good.png" />
+      </div>
+
+      <div class="item">
+        <img src="./assets/icon/line-4-right.png" />
+        <img class="tag" src="https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/site/doc/bad.png" />
+      </div>
+    </div>
+    <hr />
 
     <h3>圆角</h3>
-    <p>保持 1px 的外圆角，内角无圆角，外柔内刚。</p>
+    <p>在处理拐角时，需结合图形表意，恰当的圆角能强化视觉语言，而不是生硬地全部处理为尖角。</p>
     <img src="./assets/icon/round-1.png" alt="" />
     <p>在处理“箭头”等具有方向性的标记时，在其顶端应采用 0.5px 的圆角，以便于突出其指向性</p>
     <div class="legend">
@@ -162,12 +156,10 @@
     <div class="legend">
       <div class="item">
         <img src="./assets/icon/angle-2-left.png" />
-        <img class="tag" src="https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/site/doc/good.png" />
       </div>
 
       <div class="item">
         <img src="./assets/icon/angle-2-right.png" />
-        <img class="tag" src="https://oteam-tdesign-1258344706.cos.ap-guangzhou.myqcloud.com/site/doc/good.png" />
       </div>
     </div>
 
@@ -176,10 +168,25 @@
       断口多存在于复合图标上，其宽度根据具体图标的样式进行具体分析，需起到平衡视觉重量的作用 （最大宽度 ≤ 2px 且为 0.5px
       的倍数）
     </p>
-    <img src="./assets/icon/fracture.png" alt="" />
+    <div class="legend">
+      <div class="item">
+        <img src="./assets/icon/fracture-1-left.png" />
+      </div>
+
+      <div class="item">
+        <img src="./assets/icon/fracture-1-right.png" />
+      </div>
+    </div>
+
+    <h3>叠加型通用挂件</h3>
+    <p>为在统一规范下适配多样的主图形，提供了中、小两套通用挂件。</p>
+    <img src="./assets/icon/variety-1.png" alt="" />
+    <hr />
+    <p>使用时，需依据主图形的具体形态，选择合适尺寸并确定摆放位置。</p>
+    <img src="./assets/icon/variety-2.png" alt="" />
 
     <h3>繁简度</h3>
-    <p>在确保高识别度的情况下，寻求内部结构以及外部轮廓上的最简</p>
+    <p>在确保高识别度的基础上，寻求内部结构以及外部轮廓的最简化。</p>
     <div class="legend">
       <div class="item">
         <img src="./assets/icon/easy-1-left.png" />
@@ -205,7 +212,7 @@
     </div>
 
     <h3>弧线</h3>
-    <p>弧线处理优先遵循正圆/正圆局部拼接</p>
+    <p>弧线处理优先遵循正圆/正圆局部拼接。</p>
     <div class="legend">
       <div class="item">
         <img src="./assets/icon/arc-1-left.png" />
@@ -218,21 +225,20 @@
       </div>
     </div>
 
-    <h3>全部图标</h3>
-    <div
-      style="
-        background: #ecf2fe;
-        display: flex;
-        align-items: center;
-        line-height: 20px;
-        padding: 14px 24px;
-        border-radius: 3px;
-        color: #555a65;
-        margin: 16px 0;
-      "
+    <t-divider
+      ><div
+        style="
+          height: 60px;
+          border-radius: 999px;
+          background-color: var(--bg-color-theme-secondary);
+          display: flex;
+          align-items: center;
+          padding: 0 82px;
+        "
+      >
+        <t-link href="/icons" target="blank">打开 TDesign 图标独立站点查看全部支持的图标</t-link>
+      </div></t-divider
     >
-      打开 <a href="/icons"> TDesign 图标独立站点 </a> 查看全部支持的图标。
-    </div>
   </div>
 </template>
 
